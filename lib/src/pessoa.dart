5f9a8b27082f6b85a26429e575fc797f88559ff5
LEANDRO\Leandro L. Godoy <leandrolenon@gmail.com>
2022-02-01 16:45:18 -0300

    first commit

---
+++ lib/src/pessoa.dart
@@ -0,0 +1,14 @@
+class Pessoa {
+  String nome;
+  int idade;
+  double altura;
+  double peso;
+
+//calcular imc
+  double imc() =>  peso / (altura * altura);
+  
+
+// se é maior de idade
+  bool maiorDeIdade() => idade >= 18;
+  
+}
